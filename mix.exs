d2bde2efdf122cd4d33fa748c599c45425f7b1a2
Eduardo Gurgel <edgurgel@gmail.com>
2014-02-12 08:19:17 -0300

    Update httparrot

--- mix.exs
+++ mix.exs
@@ -4,8 +4,7 @@ defmodule HTTPoison.Mixfile do
   def project do
     [ app: :httpoison,
       version: "0.0.2",
-      elixir: "~> 0.12.3",
-      build_per_environment: false,
+      elixir: "~> 0.12.4",
       deps: deps(Mix.env) ]
   end
 
@@ -14,15 +13,12 @@ defmodule HTTPoison.Mixfile do
   end
 
   defp deps(:prod) do
-    [ 
-      { :hackney, github: "benoitc/hackney", tag: "0.10.1" }
-    ]
+    [ { :hackney, github: "benoitc/hackney", tag: "0.10.1" } ]
   end
 
   defp deps(:test) do
-    deps(:prod) ++ [ { :httparrot, github: "edgurgel/httparrot", tag: "0.0.1" },
-                     { :meck, github: "eproxus/meck", tag: "0.8.1" },
-                     { :jsex, github: "talentdeficit/jsex", override: true} ]
+    deps(:prod) ++ [ { :httparrot, github: "edgurgel/httparrot", tag: "0.0.2" },
+                     { :meck, github: "eproxus/meck", tag: "0.8.1" } ]
   end
 
   defp deps(_), do: deps(:prod)
