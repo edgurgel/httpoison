700897db7fd40c9be5a3d88dcd0d8b0d551a73bb
Eduardo Gurgel <eduardo@gurgel.me>
2014-10-14 21:34:36 +1300

    Bump version 0.5.1
    [ci skip]

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule HTTPoison.Mixfile do
 
   def project do
     [ app: :httpoison,
-      version: "0.5.0",
+      version: "0.5.1",
       elixir: "~> 1.0",
       name: "HTTPoison",
       description: @description,
