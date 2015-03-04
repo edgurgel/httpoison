3bab6035a2ba53cc708a9c659f59a3880d45f2d7
Eduardo Gurgel <eduardo@gurgel.me>
2015-03-04 18:46:40 +1300

    Bump version to 0.7.0
    [ci skip]

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule HTTPoison.Mixfile do
 
   def project do
     [ app: :httpoison,
-      version: "0.6.3",
+      version: "0.7.0",
       elixir: "~> 1.0",
       name: "HTTPoison",
       description: @description,
