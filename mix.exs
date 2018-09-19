e8963cbf9ea38a091b21b440b99b389487387571
Matt Robinson <matt@mattrobinson.net>
2018-09-19 11:45:29 -0400

    Add dialyzer checking to travis runs
    I encountered typespec errors with older versions of httpoison and it
    took me awhile to figure that out.  Great that the typespecs have all
    been fixed up in recent versions, but would be good to make that part of
    CI so it's easier to spot when there's problems in the future

--- mix.exs
+++ mix.exs
@@ -22,7 +22,19 @@ defmodule HTTPoison.Mixfile do
           "README.md",
           "CHANGELOG.md"
         ]
-      ]
+      ],
+      dialyzer: [
+        plt_add_deps: :transitive,
+        flags: [
+          # "-Wunmatched_returns",
+          # "-Wrace_conditions",
+          # "-Wunderspecs",
+          # "-Wunknown",
+          # "-Woverspecs",
+          # "-Wspecdiffs",
+        ]
+      ],
+
     ]
   end
 
@@ -37,7 +49,8 @@ defmodule HTTPoison.Mixfile do
       {:exjsx, "~> 3.1", only: :test},
       {:httparrot, "~> 1.0", only: :test},
       {:earmark, "~> 1.0", only: :dev},
-      {:ex_doc, "~> 0.14", only: :dev}
+      {:ex_doc, "~> 0.14", only: :dev},
+      {:dialyxir, "~> 1.0.0-rc.3", only: [:dev, :test], runtime: false},
     ]
   end
 
