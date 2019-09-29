377bf545c867f21c3d3c09f46bddd32ab3aa0450
Perry Cate <TheScratcherMan@gmail.com>
2019-09-29 17:21:08 -0400

    Enforce Hackney version >= 1.15.2

--- mix.exs
+++ mix.exs
@@ -43,7 +43,7 @@ defmodule HTTPoison.Mixfile do
 
   defp deps do
     [
-      {:hackney, "~> 1.15"},
+      {:hackney, ">= 1.15.2"},
       {:mimic, "~> 0.1", only: :test},
       {:exjsx, "~> 3.1", only: :test},
       {:httparrot, "~> 1.2", only: :test},
