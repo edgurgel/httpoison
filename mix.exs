ee21d004759c6dce38ec8838b9a364a90092d0de
edgurgel <eduardo@gurgel.me>
2025-02-21 06:40:43 +1300

    Bump HTTPoison version

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule HTTPoison.Mixfile do
   def project do
     [
       app: :httpoison,
-      version: "2.2.1",
+      version: "2.2.2",
       elixir: "~> 1.11",
       name: "HTTPoison",
       description: @description,
